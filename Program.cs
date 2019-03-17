d9aa7d00fc8e0192b15b6313c2fd4cbbb8143737
Colin White <clwhite814@gmail.com>
2019-03-17 15:34:38 -0500

    exercise is finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace family_dictionary
 {
@@ -6,6 +7,16 @@ namespace family_dictionary
     {
         static void Main(string[] args)
         {
+        Console.WriteLine("Be still and think");
+        Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+        myFamily.Add("dad", new Dictionary<string, string>(){ {"name", "Greg"}, {"age", "60"}});
+        myFamily.Add("mom", new Dictionary<string, string>(){ {"name", "Joycelyn"}, {"age", "57"}});
+        myFamily.Add("sister", new Dictionary<string, string>(){ {"name", "Camerin"}, {"age", "25"}});
+
+        foreach(var (key, value) in myFamily) {
+        Console.WriteLine($"{myFamily[key]["name"]} is my {key} and is {myFamily[key]["age"]}");
         }
     }
+}
+
 }
